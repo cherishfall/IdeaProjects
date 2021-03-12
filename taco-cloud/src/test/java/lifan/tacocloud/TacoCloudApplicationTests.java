a08934950ca165d2f71d146f33b8eed65e78a50c
cherishfall <cherishfall@hotmail.com>
2021-03-12 09:29:37 +0800

    spring 实战5 实现书中案例

---
+++ taco-cloud/src/test/java/lifan/tacocloud/TacoCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package lifan.tacocloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
